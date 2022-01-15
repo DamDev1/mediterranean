8703e4a74a58acbaa3df24b7c474c820a09dab57
Damilare-cloud <fridaydamilare22@gmail.com>
2022-01-15 12:01:16 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-window.onscroll = function(){myfunction}
+// window.addEventListener("scroll", function(){
+//     if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+//         document.querySelector(".changebg").style.background = "white",
+//         document.querySelector(".name").style.color = "black";
+
+//     }else if(document.body.scrollTop < 50 || document.documentElement.scrollTop < 50){
+//         document.querySelector(".changebg").style.background = "transparent"
+//         document.querySelector(".name").style.color = "white";
+//     }
+// });
